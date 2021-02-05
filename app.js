a1f0cc9059848786a9a46cd5ed0830393ff155ae
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2021-02-05 14:35:30 +0000

    battle log

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            logMessages: []
         }
     },
 
@@ -17,18 +18,21 @@ const app = Vue.createApp({
             this.currentRound++
             const attackValue = getRandomValue(5,12)
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'attack', attackValue)
             this.attackPlayer()
         },
 
         attackPlayer(){
             const attackValue = getRandomValue(8,15)
             this.playerHealth -= attackValue;
+            this.addLogMessage('monster', 'attack', attackValue)
         },
 
         specialAttackMonster(){
             this.currentRound++
             const attackValue = getRandomValue(10,25)
             this.monsterHealth -= attackValue
+            this.addLogMessage('player', 'special-attack', attackValue)
             this.attackPlayer();
         },
 
@@ -40,6 +44,7 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healValue
             };
+            this.addLogMessage('player', 'heal', healValue)
             this.attackPlayer()
         },
 
@@ -51,7 +56,17 @@ const app = Vue.createApp({
             this.playerHealth = 100,
             this.monsterHealth = 100,
             this.winner = null,
-            this.currentRound = 0
+            this.currentRound = 0,
+            this.logMessages = []
+
+        },
+
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            })
         }
     },
 
